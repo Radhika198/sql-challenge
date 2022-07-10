a5f04b2d4a22f1a7f227f65bf9a8acbca9f1137a
Radhika198 <gradhikababu@gmail.com>
2022-07-10 22:27:48 +1000

    updated

--- EmployeeSQL/employee_schema.sql
+++ EmployeeSQL/employee_schema.sql
@@ -1,42 +1,35 @@
-CREATE TABLE titles(
-	title_id VARCHAR PRIMARY KEY,
-	title VARCHAR NOT NULL
-);
+titles
+------------
+title_id PK varchar
+title varchar
 
-CREATE TABLE departments(
-	dept_no VARCHAR PRIMARY KEY,
-	dept_name VARCHAR
-);
+departments
+-----------
+dept_no PK varchar
+dept_name varchar 
 
-CREATE TABLE employees(
-	emp_no INT PRIMARY KEY,
-	emp_title_id VARCHAR NOT NULL,
-	birth_date VARCHAR NOT NULL,
-	first_name VARCHAR NOT NULL,
-	last_name VARCHAR NOT NULL,
-	sex VARCHAR NOT NULL,
-	hire_date VARCHAR NOT NULL,
-	FOREIGN KEY (emp_title_id) REFERENCES titles(title_id)
-);
+employees
+----------
+emp_no PK int
+emp_title_id varchar FK >- titles.title_id
+birth_date date
+first_name varchar
+last_name varchar
+sex varchar
+hire_date date
 
-CREATE TABLE dept_emp(
-	emp_no INT NOT NULL,
-	dept_no VARCHAR NOT NULL,
-	PRIMARY KEY (emp_no, dept_no),
-	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
-	FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
-);
+dept_emp
+----------
+emp_no PK int FK >- employees.emp_no
+dept_no PK varchar FK >- departments.dept_no
 
-CREATE TABLE salaries(
-	emp_no INT PRIMARY KEY,
-	salary INT NOT NULL,
-	FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
-);
+dept_manager
+------------
+dept_no PK varchar FK >- departments.dept_no
+emp_no PK varchar FK >- employees.emp_no
+ 
+salaries
+------------
+emp_no PK int FK >- employees.emp_no
+salary bigint
 
-CREATE TABLE dept_manager(
-	dept_no VARCHAR NOT NULL,
-	emp_no INT NOT NULL,
-	PRIMARY KEY (dept_no, emp_no),
-	FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
-	FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
-);
